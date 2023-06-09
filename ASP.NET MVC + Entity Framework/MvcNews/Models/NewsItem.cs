62a32f6a664a3d761685ab95337b1d373da0a209
Justyna Frączek <justynkaf553@gmail.com>
2023-06-09 13:19:31 +0200

    add mvc

---
+++ ASP.NET MVC + Entity Framework/MvcNews/Models/NewsItem.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcNews.Models
+{
+    public class NewsItem
+    {
+        public int Id
+        {
+            get; set;
+        }
+
+        [DataType(DataType.Date)]
+        public DateTime TimeStamp 
+        {
+            get; set;
+        }
+
+        [Required]
+        [StringLength(40, MinimumLength = 5)]
+        public string Text
+        {
+            get; set;
+        }
+
+        [Timestamp]
+        public byte[]? RowVersion 
+        { 
+            get; set; 
+        }
+    }
+}
